a0b9e991f30c1afd282111eccde20bee02720932
moidmw <moid.mw@gmail.com>
2016-06-04 15:59:32 +0530

    folder structuring and added dependencies

--- gulp-config.js
+++ gulp-config.js
@@ -1,5 +1,16 @@
 module.exports = function(){
   return {
-    base:'index.html',
+    srcPath: 'src/',
+    index: srcPath + 'index.html',
+    jsIncludes:[
+      /*include all the external js libraries here*/
+    ],
+    jsFiles:[
+      /*include all you main code js files here*/
+    ],
+    cssIncludes:[
+      /*include all the external css libraries here*/
+    ],
+    mainCss: /*Main css file*/ '',
   }
 }
